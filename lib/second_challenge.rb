18519e5e55971ee461f898a46f78d8617f1c8a26
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-05-25 20:53:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,20 +1,17 @@
 def second_challenge
   epic_tragedy =
   {
-    :montague =>
-    {
+    :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => []
-    },
-     :capulet =>
-    {
+      :hero_friends => []},
+      
+     :capulet =>{
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => []
-    }
+      :heroine_friends => []}
     
   }
 end
